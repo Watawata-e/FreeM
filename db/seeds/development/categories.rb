7a201d92dc1f3082df32c469feaf4033a237a61b
Ishiwata <ne300093@senshu-u.jp>
2020-01-23 18:28:51 +0900

    heroku~~~~~

--- db/seeds/development/categories.rb
+++ db/seeds/development/categories.rb
@@ -1,4 +1,4 @@
-categories = ["服", "雑貨", "食品", "家電製品", "音楽", "その他"]
+categories = ["服", "雑貨", "食品", "家電製品", "音楽", "インテリア", "その他"]
 categories.each do |category|
   Category.create(category: category)
 end
